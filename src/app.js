a23e91f06a375128c0173091b00c3f3779dbfd45
catng0 <132963322+catng0@users.noreply.github.com>
2024-09-24 23:04:14 +0700

    Add files via upload

--- src/app.js
+++ src/app.js
@@ -1,12 +1,11 @@
 const express = require('express');
 const cors = require('cors');
-
-const JSend = require('./jsend');  // Nhập module JSend
+const JSend = require('./jsend');
 const contactsRouter = require('./routes/contacts.router');
 const {
     resourceNotFound,
     handleError,
-}   = require('./controllers/errors.controller');
+} = require('./controllers/errors.controller');
 const { specs, swaggerUi } = require('./docs/swagger');
 
 const app = express();
@@ -15,17 +14,23 @@ app.use(cors());
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
-app.get('/', (req, res) => {
-  return res.json(JSend.success());  // Sử dụng hàm success để trả về phản hồi
-});
-
+// Serve Swagger UI
 app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(specs));
+
+// Serve static files from the 'public' directory
 app.use('/public', express.static('public'));
 
+app.get('/', (req, res) => {
+    return res.json(JSend.success());
+});
+
+// Setup routes
 contactsRouter.setup(app);
 
+// Handle 404 response for undefined routes
 app.use(resourceNotFound);
 
+// Define error-handling middleware last, after other app.use() and routes calls
 app.use(handleError);
 
 module.exports = app;
